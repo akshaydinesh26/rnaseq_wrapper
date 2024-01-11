37c30eca828c875fdf1373b58b76df379c7d11ab
akshaydinesh26 <akshaydinesh26@gmail.com>
2024-01-11 20:46:16 +0530

    change: Added Functions to initialize directory         and prepare files for the pipeline run. branch: master author: akshaydinesh date  : 11/01/2024

--- wrapper.py
+++ wrapper.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python
 
 import subprocess
-from argparse import ArgumentParser
+import argparse
 import shutil
 import os
 from Bio import SeqIO
+import tempfile
 
 # arguments are transcriptome fasta and fastq file
 
+# validate input file path
+def validate_file(file):
+    if not os.path.exists(file):
+        raise argparse.ArgumentTypeError("{0} does not exist".format(file))
+    return file
 
 
-
-
-
-
-
-
-
+# Input filepath arguments
+parser = argparse.ArgumentParser(description='Transcriptome based RNAseq abundance estimation')
+parser.add_argument('-T', dest='transcriptomefile', required=True,
+                    help="Reference transcriptome file",type=validate_file)
+parser.add_argument('-R1', dest='read1', required=True,
+                    help="Fastq reads R1",type=validate_file)
+parser.add_argument('-R2', dest='read2', required=True,
+                    help="Fastq reads R2",type=validate_file)
+args = parser.parse_args()
 
 # check fasta header is unique
 def checkHeader(fasta):
@@ -32,12 +40,39 @@ def checkHeader(fasta):
 # create directory structure
 def makebin(rootpath):
   os.mkdir(rootpath)
-  resultpath = os.path.join(rootpath, "results")
+  resultpath = os.path.join(rootpath, "inputData")
   os.mkdir(resultpath)
   inputpath = os.path.join(rootpath, "results")
   os.mkdir(inputpath)
 
 
-# move files
-def move(oldPath,newPath):
-  shutil.move(oldPath,newPath)
+temp_dir = tempfile.TemporaryDirectory()
+# temp_dir.cleanup()
+# To replace strings in nextflow script
+def replacestring(inFilepath,outFilepath,toReplace,replaceWith):
+   with open(inFilepath, "rt") as fin:
+      with open(outFilepath, "wt") as fout:
+         for line in fin:
+            fout.write(line.replace(toReplace , replaceWith))
+
+
+def initialize(): 
+   if (checkHeader(args.transcriptomefile)):
+      makebin(temp_dir)
+      inputpath=os.path.join=(temp_dir,"inputData")
+      shutil.copy(args.transcriptomefile,inputpath)
+      shutil.copy(args.read1,inputpath)
+      shutil.copy(args.read2,inputpath)
+
+
+
+  
+   
+
+
+
+
+
+
+
+
